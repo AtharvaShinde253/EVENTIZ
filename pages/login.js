af43437e36096d319a821acfc359e7aa3fc73a65
Chinmay Rathod <98706872+chinmayy19@users.noreply.github.com>
2024-10-17 22:32:22 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -84,11 +84,11 @@ const login = () => {
 				<div className='login md:w-[40%] h-[100vh] relative'>
 					<div className='absolute bottom-5 right-5'>
 						<a
-							href='https://github.com/dha-stix'
+							href='https://github.com/chinmayy19'
 							target='_blank'
 							className='text-gray-100'
 						>
-							Built by David Asaolu
+							Built by Chinmay Rathod
 						</a>
 					</div>
 				</div>
